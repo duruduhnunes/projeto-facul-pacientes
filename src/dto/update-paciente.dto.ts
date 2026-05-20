a5fccd74a2370b8c73d7c8b8ffd57b80fb6d853d
duruduhnunes <duruduh883@gmail.com>
2026-05-19 21:48:11 -0300

    implementação do Swagger para documentação da API de Pacientes

--- src/dto/update-paciente.dto.ts
+++ src/dto/update-paciente.dto.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdatePacienteDto {
+  @ApiProperty({ required: false, description: 'Nome do paciente' })
   nome?: string;
+  @ApiProperty({ required: false, description: 'Email do paciente' })
   email?: string;
+  @ApiProperty({ required: false, description: 'Telefone do paciente' })
   telefone?: string;
 }
