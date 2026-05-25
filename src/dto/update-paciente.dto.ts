bbb1982f5b0210c08239d6a1f7cff61acc2ed1ec
bluishst <thatshitgrrr@gmail.com>
2026-05-25 14:43:44 -0300

    continucao do valitor e paginacao do get all

--- src/dto/update-paciente.dto.ts
+++ src/dto/update-paciente.dto.ts
@@ -1,10 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class UpdatePacienteDto {
   @ApiProperty({ required: false, description: 'Nome do paciente' })
+  @IsOptional()
+  @IsString({ message: 'O nome deve ser uma string' })
   nome?: string;
+
   @ApiProperty({ required: false, description: 'Email do paciente' })
+  @IsOptional()
+  @IsEmail({}, { message: 'Email inválido' })
   email?: string;
+
   @ApiProperty({ required: false, description: 'Telefone do paciente' })
+  @IsOptional()
+  @IsString({ message: 'O telefone deve ser uma string' })
   telefone?: string;
 }
--- src/repository/paciente.repository.ts
+++ src/repository/paciente.repository.ts
@@ -2,7 +2,10 @@ import { PacienteEntity } from 'src/entities/pacientes.entity';
 
 export abstract class PacientesRepository {
   abstract save(paciente: PacienteEntity): Promise<PacienteEntity>;
-  abstract findAll(): Promise<PacienteEntity[]>;
+  abstract findAll(
+    page: number,
+    limit: number,
+  ): Promise<{ pacientes: PacienteEntity[]; total: number }>;
   abstract findByEmail(email: string): Promise<PacienteEntity | null>;
   abstract findById(id: string): Promise<PacienteEntity | null>;
   abstract update(
